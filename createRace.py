df92ce0bf2eeafd14690e1b29c50576f67142ebf
Aetrix27 <davedogg323@gmail.com>
2020-12-07 21:16:08 -0800

    Created compostion class

--- createRace.py
+++ createRace.py
@@ -11,63 +11,71 @@ def checkWinner(firstRacer, secondRacer):
         print(f"{firstRacer.name} wins!")
     elif secondRacer.speed > firstRacer.speed and secondRacer.health > 0:
         print(f"{secondRacer.name} wins!")
+    elif firstRacer.health > secondRacer.health and firstRacer.health > 0:
+        print(f"{firstRacer.name} wins!")
+    elif secondRacer.health > firstRacer.health and secondRacer.health > 0:
+        print(f"{secondRacer.name} wins!")
     else:
         print("It's a tie!")
-        
-gameRunning = True
-itemNames = ["item1", "item2", "item3"]
-enemyItemNames = ["enemyItem1", "enemyItem2", "enemyItem3"]
 
-while gameRunning == True:
-    randEnemyClasses = [Cyborg(False, "Cybertron"), Animal(False, "Nike"), CyborgAnimal(False, "Hybrid")]   
-    enemyRacer = random.choice(randEnemyClasses)
-    species = input("Do you want to be a cyborg (110 Health and 30 speed), animal (100 Health and 35 speed), or cyborgAnimal (105 Health and 32.5 speed)?\n")
-    name = input("What will your name be?\n")
+class createRace():
+    def race(self):
+        gameRunning = True
+        itemNames = ["item1", "item2", "item3"]
+        enemyItemNames = ["enemyItem1", "enemyItem2", "enemyItem3"]
 
-    if species == "cyborg":
-        playerRacer = Cyborg(True, name)
-    elif species == "animal":
-        playerRacer = Animal(True, name)
-    elif species == "cyborgAnimal":
-        playerRacer = CyborgAnimal(True, name)
+        while gameRunning == True:
+            randEnemyClasses = [Cyborg(False, "Cybertron"), Animal(False, "Nike"), CyborgAnimal(False, "Hybrid")]   
+            enemyRacer = random.choice(randEnemyClasses)
+            species = input("Do you want to be a cyborg (110 Health and 30 speed), animal (100 Health and 35 speed), or cyborgAnimal (105 Health and 32.5 speed)?\n")
+            name = input("What will your name be?\n")
 
-    for possibleItem in range(3):
-        playerGetItemChance = random.randint(0,2)
-        playerGotItem = False
-        enemyGetItemChance = random.randint(0,2)
-        enemyGotItem = False
+            if species == "cyborg":
+                playerRacer = Cyborg(True, name)
+            elif species == "animal":
+                playerRacer = Animal(True, name)
+            elif species == "cyborgAnimal":
+                playerRacer = CyborgAnimal(True, name)
 
-        if playerGetItemChance == 0:
-            playerGotItem = True
+            for possibleItem in range(3):
+                playerGetItemChance = random.randint(0,2)
+                playerGotItem = False
+                enemyGetItemChance = random.randint(0,2)
+                enemyGotItem = False
 
-        if playerGotItem == True:
-            itemNames[possibleItem] = Item()
-            itemNames[possibleItem].randomItem(playerRacer)
-            playerRacer.addItem(itemNames[possibleItem])
-            playerRacer.totalBoost()
-            print(playerRacer.returnSpeed())
+                if playerGetItemChance == 0:
+                    playerGotItem = True
 
-        if enemyGotItem == True:
-            enemyItemNames[possibleItem] = Item()
-            enemyItemNames[possibleItem].randomItem(enemyRacer)
-            enemyRacer.addItem(enemyItemNames[possibleItem])
-            enemyRacer.totalBoost()
-            print(enemyRacer.returnSpeed())
+                if playerGotItem == True:
+                    itemNames[possibleItem] = Item()
+                    itemNames[possibleItem].randomItem(playerRacer)
+                    playerRacer.addItem(itemNames[possibleItem])
+                    playerRacer.totalBoost()
+                    print(playerRacer.returnSpeed())
 
-        if possibleItem == 2:
-            powerup1 = PowerUp()
-            playerRacer.getPowerUp(powerup1)
-            playerRacer.totalPowerUp()
-            playerRacer.returnSpeed()
-            print(playerRacer.returnSpeed())
+                if enemyGotItem == True:
+                    enemyItemNames[possibleItem] = Item()
+                    enemyItemNames[possibleItem].randomItem(enemyRacer)
+                    enemyRacer.addItem(enemyItemNames[possibleItem])
+                    enemyRacer.totalBoost()
+                    print(enemyRacer.returnSpeed())
 
-            powerup2 = PowerUp()
-            enemyRacer.getPowerUp(powerup2)
-            enemyRacer.totalPowerUp()
-            enemyRacer.returnSpeed()
-            print(enemyRacer.returnSpeed())
-        
-    checkWinner(playerRacer, enemyRacer)
+                if possibleItem == 2:
+                    powerup1 = PowerUp()
+                    playerRacer.getPowerUp(powerup1)
+                    playerRacer.totalPowerUp()
+                    playerRacer.returnSpeed()
+                    print(playerRacer.returnSpeed())
 
-    gameRunning = False
+                    powerup2 = PowerUp()
+                    enemyRacer.getPowerUp(powerup2)
+                    enemyRacer.totalPowerUp()
+                    enemyRacer.returnSpeed()
+                    print(enemyRacer.returnSpeed())
+                
+            checkWinner(playerRacer, enemyRacer)
 
+            gameRunning = False
+
+newRace = createRace()
+newRace.race()
